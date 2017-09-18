0de36d16a6085df8f7611b75e9663de31f3080f2
kristina tsoleva <kristina.tsoleva@imperiamobile.com>
2017-09-18 17:48:34 +0300

    listing

--- AngularProject/src/app/restaurant-listing/restaurant-listing.component.ts
+++ AngularProject/src/app/restaurant-listing/restaurant-listing.component.ts
@@ -8,6 +8,33 @@ import {
 })
 export class RestaurantListComponent implements OnInit {
 
+  public restaurantList = [
+    {
+      id: 1,
+      name :'Red Rooster Restaurant',
+      image: 'https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg',
+      address: 'ул. Цар Калоян 1А',
+      raiting: '4/5',
+      raitingStats: {
+        food: '5/5',
+        place: '3/5',
+        staff: '5/5'
+      }
+    },
+    {
+      id: 2,
+      name :'Happy bar and grill',
+      image: 'https://beebom-redkapmedia.netdna-ssl.com/wp-content/uploads/2016/01/Reverse-Image-Search-Engines-Apps-And-Its-Uses-2016.jpg',
+      address: 'ул. Цар Калоян 1А',
+      raiting: '4/5',
+      raitingStats: {
+        food: '5/5',
+        place: '3/5',
+        staff: '5/5'
+      }
+    }
+  ];
+
   public ngOnInit(): void {
     console.log('test')
   }
